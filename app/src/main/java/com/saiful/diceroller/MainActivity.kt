aab3c12889c772b221bca593e726fd22da2a5708
Saiful-Lab <saifulislam_@outlook.com>
2022-12-16 12:51:14 +0600

    dice roll

--- app/src/main/java/com/saiful/diceroller/MainActivity.kt
+++ app/src/main/java/com/saiful/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.saiful.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +11,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.rollButton)
-        rollButton.setOnClickListener {
-            Toast.makeText(this, "Dice Roll!", Toast.LENGTH_LONG).show()
-        }
+        rollButton.setOnClickListener { rollDice() }
     }
+
+    private fun rollDice() {
+        val diceRoll = Dice(6).roll()
+
+        val resultTextView: TextView = findViewById(R.id.text_view)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int = (1..numSides).random()
 }
