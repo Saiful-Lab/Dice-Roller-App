12169dabc8aff3f3f717030470b4fdbd5ab43dd4
Saiful-Lab <saifulislam_@outlook.com>
2022-12-16 12:42:02 +0600

    button interactive

--- app/src/main/java/com/saiful/diceroller/MainActivity.kt
+++ app/src/main/java/com/saiful/diceroller/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.saiful.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val rollButton: Button = findViewById(R.id.rollButton)
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "Dice Roll!", Toast.LENGTH_LONG).show()
+        }
     }
 }
